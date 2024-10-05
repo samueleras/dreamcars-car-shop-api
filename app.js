e800803dd5b8e1e07dad92f98ddc6e3d202c4844
Samuel Eras <sa.eras@gmx.de>
2024-10-05 23:22:15 +0100

    create .env and connect mongodb atlas

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 "use strict";
 
-const express = require("express");
-const mongoose = require("mongoose");
-const Car = require("./DBmodels/car");
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import Car from "./DBmodels/car.js";
+
+dotenv.config();
 
 // express app
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS for all origins
+/* app.use(cors()); */
 
 // connect to MongoDb
-const dbURI = require("./modules/mongoDbLogin.js");
-mongoose
-  .connect(dbURI)
+await mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("connected to db"))
   .catch((err) => console.log("Couldn't connect to db" + err));
 
 // listen for requests on localhost:3000
-app.listen(3000);
+app.listen(PORT);
 
 // Use body parser for json or url
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+/* app.use(bodyParser.json()); //maybe use: app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); */
 
 // static files
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //Send static react page
+  const cars = [
+    { id: 1, brand: "Toyota", model: "Corolla" },
+    { id: 2, brand: "Honda", model: "Civic" },
+  ];
+  res.json(cars);
 });
 
 // Send all car objects
